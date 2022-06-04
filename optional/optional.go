f73c33c88f5a94021428ff5c564fdc09b0cdc82e
Stefan <jagreen@gmx.de>
2022-06-04 08:20:56 +0200

    add optional to new location

---
+++ optional/optional.go
@@ -0,0 +1,64 @@
+package optional
+
+type Optional[T any] interface {
+	Option
+	Val(orElse ...func() T) T
+}
+
+type Option interface {
+	IsSome() bool
+	IsNone() bool
+}
+
+func New[V any](v V, isSome bool) Optional[V] {
+	return Value[V]{isSome, v}
+}
+
+func None[V any]() Optional[V] {
+	return Value[V]{
+		val:    getZero[V](),
+		isSome: false,
+	}
+}
+
+type Value[T any] struct {
+	isSome bool
+	val    T
+}
+
+func (v Value[T]) IsNone() bool {
+	return !v.isSome
+}
+
+func (v Value[T]) IsSome() bool {
+	return v.isSome
+}
+
+func (v Value[T]) Val(orElse ...func() T) T {
+	if v.IsNone() {
+		if orElse != nil {
+			return orElse[0]()
+		}
+		panic("trying to call Val() on None")
+	}
+
+	return v.val
+}
+
+func getZero[T any]() T {
+	var result T
+	return result
+}
+
+func OrElse[T any](v T) func() T {
+	return func() T {
+		return v
+	}
+}
+
+func OrZero[T any]() func() T {
+	return func() T {
+		var result T
+		return result
+	}
+}
---
+++ optional/optional_test.go
@@ -0,0 +1,43 @@
+package optional
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNone(t *testing.T) {
+	t.Run("None is not Some", func(t *testing.T) {
+		n := None[string]()
+		if got := n.IsSome(); got != false {
+			t.Errorf("IsSome() = %v, want %v", got, false)
+		}
+	})
+
+	t.Run("None is None", func(t *testing.T) {
+		n := None[int]()
+		if got := n.IsNone(); got != true {
+			t.Errorf("IsNone() = %v, want %v", got, true)
+		}
+	})
+}
+
+func TestNew(t *testing.T) {
+	t.Run("New of None returns None", func(t *testing.T) {
+		none := None[int]()
+		got := New(none, true)
+
+		assert.True(t, got.IsSome())
+		assert.False(t, got.Val().IsSome())
+	})
+}
+
+func TestNone_Val(t *testing.T) {
+	t.Run("Val of None", func(t *testing.T) {
+		n := None[string]()
+
+		assert.Panics(t, func() { _ = n.Val() })
+		assert.Equal(t, "hello", n.Val(OrElse("hello")))
+		assert.Equal(t, "", n.Val(OrZero[string]()))
+	})
+}
